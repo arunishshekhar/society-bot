f6e8ebf16445e2f10a495394fb0be5a8dd425996
Arunish Shekhar <arunishshekhar@hotmail.com>
2026-05-30 17:16:26 +0530

    chore: remove obsolete database trigger utilities and Telegram test scripts

--- apps/bot/test-telegram.ts
+++
@@ -1,6 +0,0 @@
-import { Telegraf } from 'telegraf';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
-
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
-bot.telegram.sendMessage(842431611, "Society Notice\n\ntest", { parse_mode: 'MarkdownV2' }).then(() => console.log("Sent")).catch(err => console.error("Error sending:", err.message || err));
